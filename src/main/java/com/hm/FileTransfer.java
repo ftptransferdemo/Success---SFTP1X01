dde673a6d1a0c6fcbe44ff30212b6f6234630f1f
sekharp443 <rajasekhar.reddy@happiestminds.com>
2016-03-08 14:38:47 +0530

    initial commit

--- src/main/java/com/hm/FileTransfer.java
+++ src/main/java/com/hm/FileTransfer.java
@@ -1,13 +1,11 @@
 package com.hm;
 import org.apache.commons.net.ftp.FTPClient;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 
 import javax.ws.rs.Consumes;
@@ -21,7 +19,9 @@ import javax.ws.rs.POST;
 import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
 
 @Path("sendFiles")
 public class FileTransfer {
@@ -35,12 +35,6 @@ public class FileTransfer {
     {     
     	System.out.println("file name is **** "+filename);
     	// variable to connect to sftp
-    /*	String SFTPHOST = "182.19.57.244";
-    	int    SFTPPORT = 22;
-    	String SFTPUSER = "Cloudlendingturner";
-    	String SFTPPASS = "Cloudlending@321";
-    	String SFTPWORKINGDIR = "/FTP/";
-    	  */
     	String SFTPHOST=System.getenv("SFTPHOST");
     	int    SFTPPORT = Integer.valueOf(System.getenv("SFTPPORT"));
     	String SFTPUSER=System.getenv("SFTPUSER");
@@ -52,16 +46,10 @@ public class FileTransfer {
     	ChannelSftp channelSftp = null;
 
     	
-    	///////////
-        FTPClient client = new FTPClient();
-        boolean login;
-
-    	System.out.println("started dthe services *** "+fileInputStream);
-      //  FTPClient client = new FTPClient();
-        FileInputStream fis = null;
-        int read = 0;
+    	new FTPClient();
+        System.out.println("started dthe services *** "+fileInputStream);
+      int read = 0;
         byte[] bytes = new byte[1024];
-       // boolean login;
         OutputStream out;
 		try {
 			// File file=new File("Application.csv");   
@@ -93,37 +81,28 @@ public class FileTransfer {
 	            channelSftp.cd(SFTPWORKINGDIR);
 	         //   File f = new  File("D:/test004hm.txt");
 	            channelSftp.put(new FileInputStream(file), file.getName());
-	}catch(Exception ex){
-	ex.printStackTrace();
-	}
+				}catch(JSchException ex){
+				ex.printStackTrace();
+				return "ERROR - EFTP1X01 - JSchException"+ex;
+				} catch (SftpException e) {
+					e.printStackTrace();
+					return "ERROR - EFTP1X01 - SftpException"+e;
 
-			///////////
-	        InputStream in = new FileInputStream(file);
-	        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-	        StringBuilder out1 = new StringBuilder();
-	        String line;
-	        while ((line = reader.readLine()) != null) {
-	            out1.append(line);
-	            System.out.println("*** line is "+line);
-	        }
-	        System.out.println("content in the file "+out1.toString());   //Prints the string content read from input stream
-	        reader.close();
+				}
+				} catch (FileNotFoundException e1) {
+					System.out.println("*** entered into catch block while file not found "+e1);
+		
+					e1.printStackTrace();
+					return "ERROR - EFTP1X01 - FileNotFoundException"+e1;
 
-			return out1.toString();
-			/////
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			System.out.println("*** entered into catch block while file not found "+e1);
+				} catch (IOException e) {
+					System.out.println("*** entered into catch block while IOException "+e);
+					e.printStackTrace();
+					return "ERROR - EFTP1X01 - FileNotFoundException"+e;
 
-			e1.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			System.out.println("*** entered into catch block while IOException "+e);
-
-			e.printStackTrace();
-		}  
-	
-        return "done";
+				}  
+			
+        return "Success - SFTP1X01";
     }
 
 
